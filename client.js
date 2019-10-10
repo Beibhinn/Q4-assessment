e62871f7309649a08a4d2dac3a4e2a3cda972944
Beibhinn <beibhinn.keenan@gmail.com>
2019-10-10 16:11:02 +0100

    [Customize] Display only 3 most recent news releases
    Added Handlebars helper in client.js to achieve this

--- client.js
+++ client.js
@@ -14,4 +14,18 @@ $(document).ready(function(){
 	Handlebars.registerHelper('truncate', function(text, num) {
 		return text.substr(0,num) + '...';
 	});
+
+	Handlebars.registerHelper('limit', function(releases, block) {
+		let limitNews = "",
+			offset = parseInt(block.hash.offset) || 0,
+			limit = parseInt(block.hash.limit) || 3,
+			i = (offset < releases.length) ? offset : 0,
+			j = ((limit + offset) < releases.length) ? (limit + offset) : releases.length;
+
+		for(i,j; i<j; i++) {
+			limitNews += block.fn(releases[i]);
+		}
+
+		return limitNews;
+	});
 });
